e9617f1b9fdf9f11cd193b7efa90d1d32f32e3f0
Sofiqul Islam <schoolwab@gmail.com>
2020-03-03 22:02:29 +0600

    Updates

---
+++ precache-manifest.c3167d9459a194209cd9d1e0edd04888.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebb52f0b02a37786789782b117f2ebf",
+    "url": "/redux-counter/index.html"
+  },
+  {
+    "revision": "290067c1a834847d4e98",
+    "url": "/redux-counter/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "2d7fb346479d88a2b6fa",
+    "url": "/redux-counter/static/js/2.4caa3a30.chunk.js"
+  },
+  {
+    "revision": "290067c1a834847d4e98",
+    "url": "/redux-counter/static/js/main.928efec1.chunk.js"
+  },
+  {
+    "revision": "b0078c8eb852a8f895f9",
+    "url": "/redux-counter/static/js/runtime~main.125d2423.js"
+  }
+]);
